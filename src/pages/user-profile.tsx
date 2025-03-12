266288a6fef5acfeed0a63b337f466c297b389c5
cliffrochie <cliffrochie@gmail.com>
2025-03-12 18:06:05 +0800

    working with user profile

--- src/pages/user-profile.tsx
+++ src/pages/user-profile.tsx
@@ -7,18 +7,151 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
+import { IUser } from '@/@types/user'
+import api from '@/hooks/use-api'
+import useAuthUser from "@/features/user/hooks/use-auth-user"
+
+
+
+
+
 
 
 
 export default function ProfilePage() {
+  const [currentTab, setCurrentTab] = useState('user-information')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newPasswordConfirmation, setNewPasswordConfirmation] = useState('')
+  const [errors, setErrors] = useState<any>({})
 
+  const { user, handleLogin } = useAuth()
+
+  function updateDisplayTab(tab: string) {
+    setCurrentTab(tab)
+  }
+
+  async function checkUserPassword(currentPassword: string) {
+    try {
+      const username = user ? user.username : 'guest'
+      console.log('username: ', username)
+      console.log('password: ', currentPassword)
+
+      const response = await handleLogin(username, currentPassword)
+      return response.status === 200
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+
+  async function onSubmitChangePassword(currentPassword: string, newPassword: string, newPasswordConfirm: string) {
+    try {
+      const correctCurrentPassword = await checkUserPassword(currentPassword)
+      const newPasswordsMatched = newPassword === newPasswordConfirm
+
+      if(!correctCurrentPassword) {
+        setErrors({ currentPassword: 'Invalid password' })
+      }
+      else {
+        const data = errors
+        setErrors(delete data.currentPassword)
+      }
+      
+      if(!newPasswordsMatched) {
+        setErrors({ newPassword: 'Your passwords did not match.' })
+      }
+      else {
+        const data = errors
+        setErrors(delete data.newPassword)
+      }
+
+    }
+    catch(error) {
+      console.log(error)
+    }
+    
+  }
 
   return (
     <section className="grid gap-4">
       <div className="text-xl font-semibold">User Profile</div>
-      <div>
-        <div id="buttons" className="">
-
+      <div className="grid gap-2 custom-xs:grid-cols-1 custom-sm:grid-cols-[180px,auto]">
+        <div id="tabs" className="">
+          <div className="grid justify-start custom-xs:grid-cols-2 custom-sm:grid-cols-1 gap-1">
+            <Button 
+              variant="ghost" 
+              className={ currentTab === 'user-information' ? "custom-xs:justify-center custom-sm:justify-start font-semibold bg-gray-100" : "custom-xs:justify-center custom-sm:justify-start font-normal" }
+              onClick={() => updateDisplayTab('user-information') }
+            >
+              User Information
+            </Button>
+            <Button 
+              variant="ghost" 
+              className={ currentTab === 'change-password' ? "custom-xs:justify-center custom-sm:justify-start font-semibold bg-gray-100" : "custom-xs:justify-center custom-sm:justify-start font-normal" }
+              onClick={() => updateDisplayTab('change-password') }
+            >
+              Change Password
+            </Button>
+          </div>
+        </div>
+        <div className="">
+          <div id="user-information" className={currentTab === 'user-information' ? '' : 'hidden'}>
+            <Card>
+              <CardHeader>
+                <CardTitle>User Information</CardTitle>
+                <CardDescription>
+                  Please provide an information that corresponds to the issue/concern you are currently facing, an IT Personnel will be with you shortly.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+          <div id="change-password" className={currentTab === 'change-password' ? '' : 'hidden'}>
+            <Card>
+              <CardHeader>
+                <CardTitle>Change Password</CardTitle>
+                <CardDescription>
+                  You can change your password here by providing the necessary details.
+                </CardDescription>
+                <CardContent>
+                  <div className="grid gap-4 my-5 custom-xs:w-full custom-sm:w-96">
+                    <div className="grid gap-2">
+                      <Label>Current Password</Label>
+                      <Input name="currentPassword" className="" type="password" onChange={(e) => setCurrentPassword(e.target.value)} />
+                      {errors.currentPassword && (<span className="text-sm text-red-500">{errors.currentPassword}</span>)}
+                    </div>
+                    <div className="grid gap-2">
+                      <Label>New Password</Label>
+                      <Input name="newPassword" className="" type="password" onChange={(e) => setNewPassword(e.target.value)} />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label>New Password Confirmation</Label>
+                      <Input name="newPasswordConfirmation" className="" type="password" onChange={(e) => setNewPasswordConfirmation(e.target.value)} />
+                      {errors.newPassword && (<span className="text-sm text-red-500">{errors.newPassword}</span>)}
+                    </div>
+                    <Button 
+                      className="bg-blue-500 mt-3 custom-xs:w-full custom-sm:w-24"
+                      onClick={() => onSubmitChangePassword(currentPassword, newPassword, newPasswordConfirmation) }
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                </CardContent>
+              </CardHeader>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
